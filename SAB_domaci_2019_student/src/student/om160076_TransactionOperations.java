48e273e97bdc825564f3fb77d0a24c2752278f12
miha1997 <mihailo.obradovic1997@gmail.com>
2019-06-09 11:18:29 +0200

    Update om160076_TransactionOperations.java

--- SAB_domaci_2019_student/src/student/om160076_TransactionOperations.java
+++ SAB_domaci_2019_student/src/student/om160076_TransactionOperations.java
@@ -1,10 +1,20 @@
 package student;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Calendar;
+import java.util.LinkedList;
 import java.util.List;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import operations.TransactionOperations;
+import student.jdbc.DB;
 
 public class om160076_TransactionOperations implements TransactionOperations {
 
@@ -22,8 +32,26 @@ public class om160076_TransactionOperations implements TransactionOperations {
 
 	@Override
 	public List<Integer> getTransationsForBuyer(int buyerId) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection connection=DB.getInstance().getConnection();
+        String getAll="select IdNaplata from NaplataKupac where IdKupac = ?";
+        
+        try ( Statement statement=connection.createStatement();
+        	PreparedStatement psSelect=connection.prepareStatement(getAll);){
+        	
+        	psSelect.setInt(1, buyerId);
+        	ResultSet rs = psSelect.executeQuery();
+        	
+        	List<Integer> list = new LinkedList<Integer>();
+        			        			
+            while(rs.next()){
+            	list.add(rs.getInt(1));
+            }
+            
+            return list;           
+        } catch (SQLException ex) {
+            //Logger.getLogger(om160076_TransactionOperations.class.getName()).log(Level.SEVERE, null, ex);
+        	return null;
+        }
 	}
 
 	@Override
